d58b0e0fa89684543bd54a493e22ad94b5f9c1ca
Kevin Copo <kevincopo7@gmail.com>
2024-01-11 19:37:04 -0500

    V1.02

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,9 @@ import { defineConfig } from 'astro/config';
 
 import react from "@astrojs/react";
 
+
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()]
+  integrations: [react()],
 });
