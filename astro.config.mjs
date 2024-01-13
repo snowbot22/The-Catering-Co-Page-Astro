e3ffbd7049225be957ed610a4a3c40c8b17b2bd0
Kevin Copo <kevincopo7@gmail.com>
2024-01-12 20:16:30 -0500

    V1.17

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,11 @@
 import { defineConfig } from 'astro/config';
-
 import react from "@astrojs/react";
 
-
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  integrations: [react(), sitemap()],
   site: 'https://snowbot22.github.io',
-  base: '/The-Catering-Co-Page-Astro',
+  base: '/The-Catering-Co-Page-Astro'
 });
